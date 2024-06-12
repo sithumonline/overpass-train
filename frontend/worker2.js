3667acae366acd0713a13e4c12938fd2c9e62905
Sithum Bopitiya <sithumsandeepap@gmail.com>
2024-06-12 07:13:22 +0530

    Rm commented old code

--- frontend/worker2.js
+++ frontend/worker2.js
@@ -25,32 +25,11 @@ self.onmessage = async function (event) {
     let trainLat = coordinate.lat;
     let trainLon = coordinate.lng;
 
-    // if (
-    //   index + 1 >= firstRoute.coordinates.length ||
-    //   trainLat == null ||
-    //   trainLon == null
-    // ) {
-    //   continue;
-    // }
-
-    // let trainDirection = calculateBearing(
-    //   trainLat,
-    //   trainLon,
-    //   firstRoute.coordinates[index + 1].lat,
-    //   firstRoute.coordinates[index + 1].lng
-    // );
-
     console.log(
       "firstRoute started",
       `${firstRoute.coordinates.length} / ${index}`
     );
 
-    // let nearestRailwayTrack = findNearestRailwayTrack(
-    //   railwayTracks,
-    //   trainLat,
-    //   trainLon,
-    //   trainDirection
-    // );
     let nearestRailwayTrack = findNearestRailwayTrack(
       railwayTracks,
       trainLat,
@@ -86,12 +65,6 @@ self.onmessage = async function (event) {
   self.postMessage(txtCoor);
 };
 
-// function findNearestRailwayTrack(
-//   railwayTracks,
-//   trainLat,
-//   trainLon,
-//   trainDirection
-// ) {
 function findNearestRailwayTrack(railwayTracks, trainLat, trainLon) {
   let distanceFromRailwayTrack = Number.MAX_VALUE;
   let nearestRailwayTrack = null;
@@ -99,12 +72,6 @@ function findNearestRailwayTrack(railwayTracks, trainLat, trainLon) {
   let nearestRailwayAngle = null;
 
   for (let railwayTrack of railwayTracks) {
-    // let distance = findNearestDistance(
-    //   railwayTrack,
-    //   trainLat,
-    //   trainLon,
-    //   trainDirection
-    // );
     let distance = findSecondNearestNodeCoordinates(
       railwayTrack,
       trainLat,
@@ -125,96 +92,6 @@ function findNearestRailwayTrack(railwayTracks, trainLat, trainLon) {
     angle: nearestRailwayAngle,
   };
 }
-
-/*
-function findNearestDistance(railwayTrack, trainLat, trainLon, trainDirection) {
-  let distance = Number.MAX_VALUE;
-  let coordinate = null;
-  let angle = null;
-
-  for (let i = 0; i < railwayTrack.nodes.length - 1; i++) {
-    let node1 = railwayTrack.nodes[i];
-    let node2 = railwayTrack.nodes[i + 1];
-    let lat1 = node1.lat;
-    let lon1 = node1.lon;
-    let lat2 = node2.lat;
-    let lon2 = node2.lon;
-    if (lat1 == null || lon1 == null || lat2 == null || lon2 == null) {
-      continue;
-    }
-
-    let distance1 = haversine(
-      { lat: trainLat, lon: trainLon },
-      { lat: lat1, lon: lon1 }
-    );
-    let distance2 = haversine(
-      { lat: trainLat, lon: trainLon },
-      { lat: lat2, lon: lon2 }
-    );
-    let distance3 = haversine(
-      { lat: lat1, lon: lon1 },
-      { lat: lat2, lon: lon2 }
-    );
-
-    let distanceFromLine = 0;
-    if (distance1 <= distance3 && distance2 <= distance3) {
-      let s = (distance1 + distance2 + distance3) / 2;
-      distanceFromLine =
-        (2 *
-          Math.sqrt(s * (s - distance1) * (s - distance2) * (s - distance3))) /
-        distance3;
-    } else {
-      distanceFromLine = Math.min(distance1, distance2);
-    }
-
-    if (distanceFromLine < distance) {
-      distance = distanceFromLine;
-      coordinate = { lat: lat1, lng: lon1 };
-
-      // Calculate angle between train direction and railway segment
-      let trainVector = {
-        x: Math.cos(toRad(trainDirection)),
-        y: Math.sin(toRad(trainDirection)),
-      };
-      let segmentVector = { x: lon2 - lon1, y: lat2 - lat1 };
-
-      let dotProduct =
-        trainVector.x * segmentVector.x + trainVector.y * segmentVector.y;
-      let trainVectorMagnitude = Math.sqrt(
-        trainVector.x ** 2 + trainVector.y ** 2
-      );
-      let segmentVectorMagnitude = Math.sqrt(
-        segmentVector.x ** 2 + segmentVector.y ** 2
-      );
-
-      let cosineOfAngle =
-        dotProduct / (trainVectorMagnitude * segmentVectorMagnitude);
-      angle = Math.acos(cosineOfAngle) * (180 / Math.PI);
-    }
-  }
-
-  return { distance, coordinate, angle };
-}
-
-const toRad = (x) => (x * Math.PI) / 180; // Helper function for angle conversion
-
-function toDeg(radians) {
-  return (radians * 180) / Math.PI;
-}
-
-function calculateBearing(lat1, lon1, lat2, lon2) {
-  let φ1 = toRad(lat1);
-  let φ2 = toRad(lat2);
-  let Δλ = toRad(lon2 - lon1);
-
-  let y = Math.sin(Δλ) * Math.cos(φ2);
-  let x =
-    Math.cos(φ1) * Math.sin(φ2) - Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
-  let θ = Math.atan2(y, x);
-
-  return (toDeg(θ) + 360) % 360; // Normalize to 0-360 degrees
-}
-*/
 
 function findSecondNearestNodeCoordinates(railwayTrack, trainLat, trainLon) {
   let nearestDistance = Number.MAX_VALUE;
@@ -257,13 +134,6 @@ function findSecondNearestNodeCoordinates(railwayTrack, trainLat, trainLon) {
   });
 
   return { distance: nearestDistance, coordinate: nearestCoordinate, angle };
-  //   return {
-  //     nearestDistance,
-  //     nearestCoordinate,
-  //     secondNearestDistance,
-  //     secondNearestCoordinate,
-  //     angle,
-  //   };
 }
 
 // Function to calculate the bearing (angle) from coordinate1 to coordinate2
